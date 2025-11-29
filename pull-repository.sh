c781c66fffc6f27752ba71d36d907f1aac387a5c
amirhossein <amirhosseinmobini9@gmail.com>
2025-11-29 10:53:17 +0330

    feat: :sparkles: -p flag added into make directory command

--- pull-repository.sh
+++ pull-repository.sh
@@ -9,7 +9,7 @@ if [ -d "$TARGET_DIR" ]; then
     git stash
     git -c http.extraheader="AUTHORIZATION: bearer $REPOSITORY_TOKEN" pull
 else
-    mkdir $TARGET_DIR
+    mkdir -p $TARGET_DIR
     cd $TARGET_DIR
     git clone https://$REPOSITORY_TOKEN@$REPOSITORY.git .
 fi
